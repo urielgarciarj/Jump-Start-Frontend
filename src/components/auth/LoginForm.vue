e0b820329c31e499f78588fe9532b9460dfc6bea
Vianey Nava <vianey.nava@bringitps.com>
2024-11-28 15:49:34 -0600

    Added mandatory fields and fields validation
    Signed-off-by: Vianey Nava <vianey.nava@bringitps.com>
    
    	modified:   src/components/auth/LoginForm.vue
    	modified:   src/components/auth/RegisterForm.vue

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -12,34 +12,50 @@ const user = ref({
     password: '',
 });
 
+// Variables reactivas para los campos del formulario
+const valid = ref(false);
+
+// Reglas de validación
+const emailRules = [
+  (value: string) => !!value || 'El correo electrónico es obligatorio.',
+  (value: string) => /.+@.+\..+/.test(value) || 'El correo electrónico debe ser válido.'
+];
+
+const passwordRules = [
+  (value: string) => !!value || 'La contraseña es obligatoria.'
+];
+
 const error = ref<string | null>(null);
 
 // Método para registrar usuario
 const authUser = async () => {
-    try {
-    const response = await axios.post('http://localhost:3000/users/login', user.value);
-    console.log('Usuario registrado:', response.data);
-
-    // Redirigir a la página de inicio si la solicitud es exitosa
-    router.push('/'); // Cambia la ruta según sea necesario
-
-  } catch (err) {
-    console.error('Error al iniciar sesion:', err);
-
-    // Tipar el error como AxiosError
-    const errorAxios = err as AxiosError;
-
-    // Manejar el error en función del código de estado
-    if (errorAxios.response && errorAxios.response.status === 401) {
-      error.value = 'Credenciales incorrectas';
-    } else {
-      error.value = 'Ocurrió un error inesperado. Intenta nuevamente.';
+    if (valid.value) {
+        try {
+            const response = await axios.post('http://localhost:3000/users/login', user.value);
+            console.log('Usuario registrado:', response.data);
+        
+            // Redirigir a la página de inicio si la solicitud es exitosa
+            router.push('/'); // Cambia la ruta según sea necesario
+        
+        } catch (err) {
+            console.error('Error al iniciar sesion:', err);
+        
+            // Tipar el error como AxiosError
+            const errorAxios = err as AxiosError;
+        
+            // Manejar el error en función del código de estado
+            if (errorAxios.response && errorAxios.response.status === 401) {
+            error.value = 'Credenciales incorrectas';
+            } else {
+            error.value = 'Ocurrió un error inesperado. Intenta nuevamente.';
+            }
+        }
     }
-  }
 };
 </script>
 
 <template>
+<v-form v-model="valid" @submit.prevent="authUser">
     <v-row class="d-flex mb-3">
         <v-col cols="12">
             <v-alert v-if="error" type="error" dismissible>
@@ -48,14 +64,15 @@ const authUser = async () => {
         </v-col>
         <v-col cols="12">
             <v-label class="font-weight-bold mb-1">Correo electrónico</v-label>
-            <v-text-field v-model="user.email" variant="outlined" density="compact" type="email" hide-details color="primary"></v-text-field>
+            <v-text-field v-model="user.email" :rules="emailRules"  variant="outlined" density="compact" type="email" color="primary" required></v-text-field>
         </v-col>
         <v-col cols="12">
             <v-label class="font-weight-bold mb-1">Contraseña</v-label>
-            <v-text-field v-model="user.password" variant="outlined"  density="compact" type="password"   hide-details color="primary"></v-text-field>
+            <v-text-field v-model="user.password" :rules="passwordRules" variant="outlined"  density="compact" type="password" color="primary" required></v-text-field>
         </v-col>
         <v-col cols="12" class="pt-0">
-            <v-btn @click="authUser" color="primary" size="large" block   flat>Iniciar Sesión</v-btn>
+            <v-btn :disabled="!valid" @click="authUser" color="primary" size="large" block   flat>Iniciar Sesión</v-btn>
         </v-col>
     </v-row>
+</v-form>
 </template>
